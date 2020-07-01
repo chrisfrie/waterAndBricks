e339ddd0a4b2a4c12f78465d1afab54c03b09b9c
Chris <christina.friede5@gmail.com>
2020-07-01 11:34:01 +0200

    Find one solution

--- waterAndBricks.js
+++ waterAndBricks.js
@@ -1,30 +1,38 @@
-//let arr = [2, 0, 1, 1, 0, 4, 1, 3, 0, 1];
-//let arr = [2, 1, 3];
-//let arr = [2, 0, 3, 0, 4];
-let arr = [2, 0, 1, 2, 3, 0, 1, 0, 3, 1, 2, 1];
+let arr = [2, 0, 1, 2, 3, 0, 0, 1, 0, 3, 1, 0, 2, 1]; // Solution: 17
+//let arr = [2, 1, 0, 3]; // Solution: 3
+//let arr = [2, 0, 3, 0, 4]; // Solution: 5
+let result = 0;
 
-// Ermittlung des Maximalwertes
-let count = 0;
-let maxValueArr = Math.max(...arr);
-console.log("value of max: " + maxValueArr);
+console.log("Das Ausgans-Array lautet: " + arr);
 
-// wie oft gibt es den Maixmalwert
-for (let i = 0; i < arr.length; i++){
-    (arr[i] == maxValueArr) ? count += 1 : count += 0;
-};
-console.log("count of max: " + count);
 
-// Grenze rechts und linke
-let arrIndexZero = 0;
-let borderEndIndex = arr.findIndex(element => element == maxValueArr);
-console.log("borderEnd "+ borderEndIndex);
+for (let i = 1; i < arr.length - 1 ; i++){
+    console.log("Value von i ist: " + arr[i]);
+    console.log("------------------");
+    let subArrLeft = arr.slice(0, i);
+    console.log(subArrLeft);
+    let maxValueLeft = Math.max(...subArrLeft);
+    console.log(maxValueLeft);
+    console.log("------------------");
+    let subArrRight = arr.slice(i+1);
+    console.log(subArrRight);
+    let maxValueRight = Math.max(...subArrRight);
+    console.log(maxValueRight);
+    console.log("------------------");
+    console.log("------------------");
 
-for (let i = 0; i < count; i++){
-    
-    let subArr = arr.slice(arrIndexZero, borderEndIndex);
-    console.log(subArr);
-    
-    let arrNewNew = arr.slice(borderEndIndex, arr.findIndex(element => element > arr[borderEndIndex]));
-    console.log(arrNewNew);
-};
-
+    if(maxValueLeft >= maxValueRight && arr[i] <= maxValueRight){
+        result += maxValueRight - arr[i];
+    }
+    else if (maxValueLeft < maxValueRight && arr[i] <= maxValueLeft){
+            result += maxValueLeft - arr[i];
+    }
+    else {
+        result += 0;
+    }
+    console.log("Das Result ist: " + result);
+    console.log("------------------");
+    console.log("------------------");
+    console.log("------------------");
+}
+console.log(result);
